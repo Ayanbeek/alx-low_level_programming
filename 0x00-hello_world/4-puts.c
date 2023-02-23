40f0f6eef9dc548625c7a2a5d4929cdfb905e68c
Ayanbeek <ayanbeek23@gmail.com>
2023-02-23 18:10:31 +0000

    hello world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry level
+ * Description: Print file without printf function
+ * Return: Return 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
